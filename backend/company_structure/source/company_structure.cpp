ae0080f60d52c43282de22862f3f770c60d5a642
TimasTT <timofey.konoplyov@gmail.com>
2021-08-10 23:49:00 +0300

    add QTproject structure

---
+++ backend/company_structure/source/company_structure.cpp
@@ -0,0 +1,4 @@
+//
+// Created by timas on 10.08.2021.
+//
+
---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by timas on 10.08.2021.
+//
+
+#include "main_window.h"
+
+#include <QApplication>
+
+int main(int argc, char* argv[]) {
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
